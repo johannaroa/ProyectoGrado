65fcbece9c7e6f80dbbd69349d6dceb4c1bb9ad3
Alonso <alons45@aol.com>
2016-01-22 22:04:36 -0500

    Add README, examples class and others folders

---
+++ Assets/AIE3D Assets/Scripts/Objects/AnimalProperties.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimalProperties
+{
+	public Sprite miSprite;
+	public string Nombre;
+	public string SaludoAnimal;
+	public Color ColorAnimal;
+
+	public AnimalProperties(string nombreNuevo)
+	{
+		this.Nombre = nombreNuevo;
+	}
+
+	public void Habla(string texto)
+	{
+		Debug.Log(texto + SaludoAnimal);
+	}
+
+	public void Callate(TextMesh texto)
+	{
+		texto.text = "sssh";
+	}
+}
---
+++ Assets/AIE3D Assets/Scripts/Objects/Dog.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dog : AnimalProperties
+{
+	private float fuerzaSalto;
+
+	public Dog(string nuevoNombre, string raza) : base(nuevoNombre)
+	{
+		SaludoAnimal = "¡GUAU!";
+		ColorAnimal = Color.red;
+		fuerzaSalto = 200;
+		Debug.Log(raza);
+	}
+
+	public void Saltar()
+	{
+		Debug.Log (Nombre + " le dio por saltar." + fuerzaSalto);
+	}
+}
