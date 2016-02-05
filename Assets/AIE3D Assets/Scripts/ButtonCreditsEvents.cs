1995dbd7f8672f316d721dbad0308a225659d102
Alonso <alons45@aol.com>
2016-02-05 16:14:49 -0500

    Add movement to main menu

---
+++ Assets/AIE3D Assets/Scripts/ButtonCreditsEvents.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonCreditsEvents : MonoBehaviour {
+
+	public float speedCameraMovement = 20;
+	public GameObject objectToMoved;
+	public GameObject objectToFinish;
+
+	private bool applyMovement = false;
+	private float step;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveToCredits ();
+	}
+
+	void MoveToCredits() {
+		if (applyMovement == true) {
+
+			step = speedCameraMovement * Time.deltaTime;
+			objectToMoved.transform.position = Vector3.MoveTowards (objectToMoved.transform.position, objectToFinish.transform.position, step);
+		}
+
+	}
+
+	// TODO: mejorar este método ahora que defina bien el tema de organización de los Scripts
+	public void SetApplyMovement() {
+
+		GameObject search = GameObject.Find ("Search");
+		GameObject credits = GameObject.Find ("Credits");
+		GameObject creditsText = GameObject.Find ("CreditsText");
+
+		search.SetActive (false);
+		credits.SetActive (false);
+
+		Text scriptText = creditsText.GetComponent<Text>();
+		scriptText.enabled = true;
+
+		applyMovement = true;
+
+	}
+}
