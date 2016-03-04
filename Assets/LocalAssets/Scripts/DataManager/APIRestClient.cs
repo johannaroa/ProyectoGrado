0069a32b755518fa18f3610be5488cf204430659
Alonso Montenegro <alons45@aol.com>
2016-03-04 16:56:34 -0500

    Implemented skeleton for DataManager and used new Class for API REST connection

---
+++ Assets/LocalAssets/Scripts/DataManager/APIRestClient.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.Networking;
+using System.Collections;
+
+public class APIRestClient: ScriptableObject {
+
+	private string url_server = "http://localhost:8000";
+
+	public IEnumerator SearchArticle(string query){
+		string endPoint = "/forest/v1/articles/?search=";
+
+		UnityWebRequest webRequest = UnityWebRequest.Get(url_server + endPoint + query);
+		yield return webRequest.Send ();
+
+		if (webRequest.isError) {
+			Debug.Log (webRequest.error);
+		} else {
+			Debug.Log (webRequest.downloadHandler.text);
+		}
+	}
+
+	public IEnumerator AddArticle() {
+		string endPoint = "/forest/v1/articles/";
+
+		WWWForm form = new WWWForm ();
+		form.AddField("name", "Test");
+
+		UnityWebRequest webRequest = UnityWebRequest.Post (url_server + endPoint, form);
+		yield return webRequest.Send ();
+
+		if (webRequest.isError) {
+			Debug.Log (webRequest.error);
+		} else {
+			Debug.Log ("Post completed!");
+		}
+	}
+
+	IEnumerator AddCategory() {
+		yield return null;
+	}
+
+	IEnumerator AddTheme() {
+		yield return null;
+	}
+
+	IEnumerator UpdateArticle() {
+
+		string endPoint = "";
+
+		string myData = "";
+		UnityWebRequest webRequest = UnityWebRequest.Put (url_server + endPoint, myData);
+		yield return webRequest.Send() ;
+
+		if(webRequest.isError) {
+			Debug.Log(webRequest.error);
+		}
+		else {
+			Debug.Log("Put completed!");
+		}
+	}
+
+	IEnumerator UpdateCategory() {
+		yield return null;
+	}
+
+	IEnumerator UpdateTheme() {
+		yield return null;
+	}
+
+	IEnumerator DeleteArticle() {
+	
+		string endPoint = "";
+
+		UnityWebRequest webRequest = UnityWebRequest.Delete (url_server + endPoint);
+		yield return webRequest.Send ();
+
+		if(webRequest.isError) {
+			Debug.Log(webRequest.error);
+		}
+		else {
+			Debug.Log("Delete completed!");
+		}
+	}
+
+	IEnumerator DeleteCategory() {
+		yield return null;
+	}
+
+	IEnumerator DeleteTheme() {
+		yield return null;
+	}
+
+}
---
+++ Assets/LocalAssets/Scripts/Events/SearchFieldEvents.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SearchFieldEvents : MonoBehaviour {
+
+	private void SubmitSearch(string query)
+	{
+		APIRestClient apiRestClient = ScriptableObject.CreateInstance ("APIRestClient") as APIRestClient;
+
+		StartCoroutine (apiRestClient.SearchArticle(query));
+	}
+
+	private void InitListener() {
+		// TODO: Probar si en vez de poner esto aquí, que resultado da usar el evento OnEndEdit (en el GUI)
+		InputField input = gameObject.GetComponent<InputField>();
+		var se = new InputField.SubmitEvent();
+		se.AddListener(SubmitSearch);
+		input.onEndEdit = se;
+	}
+
+	// Use this for initialization
+	void Start () {
+		InitListener ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
