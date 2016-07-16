de917a56312dbeefb718dc4aa239dea6b62f3414
amarok <alons45@aol.com>
2016-07-15 23:07:53 -0500

    Remove correctly

--- Assets/LocalAssets/Scripts/Events/ButtonDeleteEvents.cs
+++ Assets/LocalAssets/Scripts/Events/ButtonDeleteEvents.cs
@@ -7,17 +7,24 @@ public class ButtonDeleteEvents : MonoBehaviour {
 		APIRestClient apiRestClient = ScriptableObject.CreateInstance ("APIRestClient") as APIRestClient;
 
 		int id = PlayerPrefs.GetInt("leafId");
-
 		StartCoroutine (apiRestClient.DeleteArticle(id));
-		Destroy(GameObject.Find ("Leaf3(Clone)-id" + id));
+
+		GameObject[] leavesWithTag = GameObject.FindGameObjectsWithTag ("Leaf");
+
+		foreach(GameObject leafWithTag in leavesWithTag) {
+			if (leafWithTag.name == "Leaf3(Clone)-id" + id) {
+				Destroy(leafWithTag);
+			}
+		}
+
 	}
 
 	public void DeleteBranch() {
 		APIRestClient apiRestClient = ScriptableObject.CreateInstance ("APIRestClient") as APIRestClient;
 	
 		int id = PlayerPrefs.GetInt ("branchId");
-
 		StartCoroutine (apiRestClient.DeleteCategory(id));
+
 		Destroy(GameObject.Find ("Branch1(Clone)-id" + id));
 	}
 
@@ -25,8 +32,8 @@ public class ButtonDeleteEvents : MonoBehaviour {
 		APIRestClient apiRestClient = ScriptableObject.CreateInstance ("APIRestClient") as APIRestClient;
 
 		int id = PlayerPrefs.GetInt ("trunkId");
-
 		StartCoroutine (apiRestClient.DeleteThematic(id));
+
 		Destroy(GameObject.Find ("Trunk(Clone)-id" + id));
 	}
 
